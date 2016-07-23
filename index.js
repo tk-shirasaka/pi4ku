a19b63ea9a5ccb3856c27b579d9060d5e38d5c8b
shirasaka <shirasaka@n-create.co.jp>
2016-07-23 16:26:35 +0900

    デザイン調整

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
 app.get('/', (req, res) => {
 	res.render('index', {
-		title: 'Test',
+		title: 'Pi Moniter',
 		user: req.query.user,
 		exists: !!users[req.query.user]
 	});
